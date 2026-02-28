dee4fc1339e682ea91b051976b0b46a06895cae5
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-02-28 15:38:13 -0300

    feat: add teams data for memory game

---
+++ src/data/teams.ts
@@ -0,0 +1,108 @@
+type Team = {
+    id: number,
+    name: string,
+    image: string
+}
+
+export const teams: Team[] = [
+    {
+        id: 1,
+        name: 'Athletico-PR',
+        image: '/athletico-pr.png'
+    },
+    {
+        id: 2,
+        name: 'Atlético-MG',
+        image: '/atletico.png'
+    },
+    {
+        id: 3,
+        name: 'Bahia',
+        image: '/bahia.png'
+    },
+    {
+        id: 4,
+        name: 'Botafogo',
+        image: '/botafogo.png'
+    },
+    {
+        id: 5,
+        name: 'Bragantino',
+        image: '/bragantino.png'
+    },
+    {
+        id: 6,
+        name: 'Chapecoense',
+        image: '/chapecoense.svg.webp'
+    },
+    {
+        id: 7,
+        name: 'Corinthians',
+        image: '/corinthians.png'
+    },
+    {
+        id: 8,
+        name: 'Coritiba',
+        image: '/coritiba.png'
+    },
+    {
+        id: 9,
+        name: 'Cruzeiro',
+        image: '/cruzeiro.png'
+    },
+    {
+        id: 10,
+        name: 'Flamengo',
+        image: '/flamengo.png'
+    },
+    {
+        id: 11,
+        name: 'Fluminense',
+        image: '/fluminense.png'
+    },
+    {
+        id: 12,
+        name: 'Grêmio',
+        image: '/gremio.png'
+    },
+    {
+        id: 13,
+        name: 'Internacional',
+        image: '/internacional.png'
+    },
+    {
+        id: 14,
+        name: 'Mirassol',
+        image: '/mirassol.png'
+    },
+    {
+        id: 15,
+        name: 'Palmeiras',
+        image: '/palmeiras.png'
+    },
+    {
+        id: 16,
+        name: 'Remo',
+        image: '/remo.png'
+    },
+    {
+        id: 17,
+        name: 'Santos',
+        image: '/santos.png.webp'
+    },
+    {
+        id: 18,
+        name: 'São Paulo',
+        image: '/sao-paulo.png'
+    },
+    {
+        id: 19,
+        name: 'Vasco da Gama',
+        image: '/vasco.png'
+    },
+    {
+        id: 20,
+        name: 'Vitória',
+        image: '/vitoria.png'
+    }
+]
