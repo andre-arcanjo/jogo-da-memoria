fbfe238272e3d11e530c8ee6237622e29021f058
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-03-30 19:19:52 -0300

    style: modification in stylling

--- src/App.tsx
+++ src/App.tsx
@@ -76,27 +76,27 @@ function App() {
   return (
     <div className="min-h-screen space-y-4 flex flex-col justify-center items-center">
       <h1 className="text-3xl">Jogo da Memória</h1>
-      <h2 className="text-2xl">Tema: Times Brasileirão Serie <span className="text-green-700">A</span> 2026</h2>
+      <h2 className="sm:text-2xl">Tema: Times Brasileirão Serie <span className="text-green-700">A</span> 2026</h2>
       <div className='flex flex-wrap justify-center gap-4'>
         {
           cards.map(time => (
             <button onClick={() => handleCardClick(time.id)} key={time.id} className='flex flex-col items-center gap-3 cursor-pointer'>
               {time.flipped ?
                 (
-                  <div className="w-32 h-32 flex flex-col items-center justify-center gap-2">
+                  <div className="w-32 h-32 bg-gray-200 flex flex-col items-center justify-center gap-2 rounded-lg">
                     <p>{time.name}</p>
                     <img src={time.image} alt={time.name} className='w-14 h-14' />
                   </div>
                 ) :
                 (
-                  <div className="bg-gray-600 w-32 h-32 rounded-lg">
+                  <div className="bg-gray-600 w-32 h-32 rounded-lg hover:bg-gray-800">
                   </div>
                 )}
             </button>
           ))
         }
         {hasWon && 
-        <h1 className="text-2xl">Você venceu! 🎉  <a className="bg-green-700 text-white p-1 rounded-lg hover:bg-green-500 transition-all" href="/">Começar novamente</a></h1>
+        <h1 className="sm:text-2xl">Você venceu! 🎉  <a className="bg-green-700 text-white p-1 rounded-lg hover:bg-green-500 transition-all" href="/">Começar novamente</a></h1>
         }
       </div>
     </div>
@@ -104,3 +104,8 @@ function App() {
 }
 
 export default App
+
+//segunda:subir pro git alterações na estilização
+// terça: subir projeto versão final
+// quarta: fazer readme.md e postar no linkedin
+// futuramente: contador de tentativas e cronometro
