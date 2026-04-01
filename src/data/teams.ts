57b443f2a69c0eea789fa52cd73bc69401ac9e17
andre-arcanjo <andrearcanjosouza@hotmail.com>
2026-03-31 21:20:04 -0300

    fix: correct image paths for production build

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -10,140 +10,140 @@ export const teams: Team[] = [
     {
         id: 1,
         name: 'Athletico-PR',
-        image: '/athletico-pr.png',
+        image: 'athletico-pr.png',
         flipped: false,
         matched: false
     },
     {
         id: 2,
         name: 'Atlético-MG',
-        image: '/atletico.png',
+        image: 'atletico.png',
         flipped: false,
         matched: false
     },
     {
         id: 3,
         name: 'Bahia',
-        image: '/bahia.png',
+        image: 'bahia.png',
         flipped: false,
         matched: false
     },
     {
         id: 4,
         name: 'Botafogo',
-        image: '/botafogo.png',
+        image: 'botafogo.png',
         flipped: false,
         matched: false
     },
     {
         id: 5,
         name: 'Bragantino',
-        image: '/bragantino.png',
+        image: 'bragantino.png',
         flipped: false,
         matched: false
     },
     {
         id: 6,
         name: 'Chapecoense',
-        image: '/chapecoense.svg.webp',
+        image: 'chapecoense.svg.webp',
         flipped: false,
         matched: false
     },
     {
         id: 7,
         name: 'Corinthians',
-        image: '/corinthians.png',
+        image: 'corinthians.png',
         flipped: false,
         matched: false
     },
     {
         id: 8,
         name: 'Coritiba',
-        image: '/coritiba.png',
+        image: 'coritiba.png',
         flipped: false,
         matched: false
     },
     {
         id: 9,
         name: 'Cruzeiro',
-        image: '/cruzeiro.png',
+        image: 'cruzeiro.png',
         flipped: false,
         matched: false
     },
     {
         id: 10,
         name: 'Flamengo',
-        image: '/flamengo.png',
+        image: 'flamengo.png',
         flipped: false,
         matched: false
     },
     {
         id: 11,
         name: 'Fluminense',
-        image: '/fluminense.png',
+        image: 'fluminense.png',
         flipped: false,
         matched: false
     },
     {
         id: 12,
         name: 'Grêmio',
-        image: '/gremio.png',
+        image: 'gremio.png',
         flipped: false,
         matched: false
     },
     {
         id: 13,
         name: 'Internacional',
-        image: '/internacional.png',
+        image: 'internacional.png',
         flipped: false,
         matched: false
     },
     {
         id: 14,
         name: 'Mirassol',
-        image: '/mirassol.png',
+        image: 'mirassol.png',
         flipped: false,
         matched: false
     },
     {
         id: 15,
         name: 'Palmeiras',
-        image: '/palmeiras.png',
+        image: 'palmeiras.png',
         flipped: false,
         matched: false
     },
     {
         id: 16,
         name: 'Remo',
-        image: '/remo.png',
+        image: 'remo.png',
         flipped: false,
         matched: false
     },
     {
         id: 17,
         name: 'Santos',
-        image: '/santos.png.webp',
+        image: 'santos.png.webp',
         flipped: false,
         matched: false
     },
     {
         id: 18,
         name: 'São Paulo',
-        image: '/sao-paulo.png',
+        image: 'sao-paulo.png',
         flipped: false,
         matched: false
     },
     {
         id: 19,
         name: 'Vasco da Gama',
-        image: '/vasco.png',
+        image: 'vasco.png',
         flipped: false,
         matched: false
     },
     {
         id: 20,
         name: 'Vitória',
-        image: '/vitoria.png',
+        image: 'vitoria.png',
         flipped: false,
         matched: false
     }
